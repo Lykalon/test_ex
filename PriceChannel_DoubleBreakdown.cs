e8ed606937ef22bcbafd045a95e85b9a7629da17
Lykalon <lykalon@student.21-school.ru>
2021-12-23 14:15:42 +0300

    Update PriceChannel_DoubleBreakdown.cs
    Переделана логика по Reverse стратегии. Для реверса достаточно поменять знаки +/- в предыдущей логике, и разнести код под управление if

--- PriceChannel_DoubleBreakdown.cs
+++ PriceChannel_DoubleBreakdown.cs
@@ -30,13 +30,7 @@ namespace ModulSolution.Robots
         {
             if (Reverse.ValueBool)
             {
-                _dir = "Short";
-            }
-            else
-            {
-                _dir = "Long";
-            }
-            for (var bar = IndexBar; bar < CandleCount - 1; bar++)
+                for (var bar = IndexBar; bar < CandleCount - 1; bar++)
             {
                 if (ChannelUpLittle.param.LinesIndicators[0].LineParam[0].Value < bar || ChannelUpBig.param.LineIndicators[0].LineParam[0].Value < bar)
                 {
@@ -50,24 +44,24 @@ namespace ModulSolution.Robots
                         if (ShortPos.Count == 0 && _dir != "Short" && Math.Abs(priceUpBig[bar + 1] - priceUpLittle[bar + 1]) <= DistMax.Value
                                 && Math.Abs(priceUpBig[bar + 1] - priceUpLittle[bar + 1]) >= DistMin.Value && priceUpLittle[bar + 1] - priceDownLittle[bar + 1] <= HLittleMax.Value
                                 && (priceDownBig[bar + 1] - priceUpLittle[bar + 1] > 0 || priceDownLittle[bar + 1] - priceUpBig[bar + 1] > 0))
-                            BuyGreater(bar + 1, priceUpLittle[bar + 1] + Proboy.ValueInt * FinInfo.Security.MinStep, 1,
+                            BuyGreater(bar + 1, priceUpLittle[bar + 1] - Proboy.ValueInt * FinInfo.Security.MinStep, 1,
                                 "Открытие длинной позиции");
                         if (LongPos.Count == 0 && _dir != "Long" && Math.Abs(priceUpBig[bar + 1] - priceUpLittle[bar + 1]) <= DistMax.Value
                                 && Math.Abs(priceUpBig[bar + 1] - priceUpLittle[bar + 1]) >= DistMin.Value && priceUpLittle[bar + 1] - priceDownLittle[bar + 1] <= HLittleMax.Value
                                 && (priceDownBig[bar + 1] - priceUpLittle[bar + 1] > 0 || priceDownLittle[bar + 1] - priceUpBig[bar + 1] > 0))
-                            ShortLess(bar + 1, priceDownLittle[bar + 1] - Proboy.ValueInt * FinInfo.Security.MinStep, 1,
+                            ShortLess(bar + 1, priceDownLittle[bar + 1] + Proboy.ValueInt * FinInfo.Security.MinStep, 1,
                                 "Открытие короткой позиции");
                     }
                     // дополнить выход из позиций по логике
                     if (ShortPos.Count > 0)
                     {
-                        CoverAtStop(bar + 1, ShortPos[0], priceUpLittle[bar + 1] + Proboy.ValueInt * FinInfo.Security.MinStep,
+                        CoverAtStop(bar + 1, ShortPos[0], priceUpLittle[bar + 1] - Proboy.ValueInt * FinInfo.Security.MinStep,
                             "Переворот.");                 
                     }
                     // дополнить выход из позиций по логике
                     if (LongPos.Count > 0)
                     {
-                        SellAtStop(bar + 1, LongPos[0], priceDownLittle[bar + 1] - Proboy.ValueInt * FinInfo.Security.MinStep,
+                        SellAtStop(bar + 1, LongPos[0], priceDownLittle[bar + 1] + Proboy.ValueInt * FinInfo.Security.MinStep,
                             "Переворот.");
                     }
 
@@ -102,6 +96,77 @@ namespace ModulSolution.Robots
                     ParamDebug("Направление", _dir);
                 }
             }
+            }
+            else
+            {
+                for (var bar = IndexBar; bar < CandleCount - 1; bar++)
+                {
+                    if (ChannelUpLittle.param.LinesIndicators[0].LineParam[0].Value < bar || ChannelUpBig.param.LineIndicators[0].LineParam[0].Value < bar)
+                    {
+                        var priceUpLittle = ChannelUpLittle.param.LinesIndicators[0].PriceSeries;
+                        var priceDownLittle = ChannelUpLittle.param.LinesIndicators[1].PriceSeries;
+                        var priceUpBig = ChannelUpBig.param.LinesIndicators[0].PriceSeries;
+                        var priceDownBig = ChannelUpBig.param.LinesIndicators[1].PriceSeries;
+
+                        if (LongPos.Count == 0 && ShortPos.Count == 0)
+                        {
+                            if (ShortPos.Count == 0 && _dir != "Short" && Math.Abs(priceUpBig[bar + 1] - priceUpLittle[bar + 1]) <= DistMax.Value
+                                    && Math.Abs(priceUpBig[bar + 1] - priceUpLittle[bar + 1]) >= DistMin.Value && priceUpLittle[bar + 1] - priceDownLittle[bar + 1] <= HLittleMax.Value
+                                    && (priceDownBig[bar + 1] - priceUpLittle[bar + 1] > 0 || priceDownLittle[bar + 1] - priceUpBig[bar + 1] > 0))
+                                BuyGreater(bar + 1, priceUpLittle[bar + 1] + Proboy.ValueInt * FinInfo.Security.MinStep, 1,
+                                    "Открытие длинной позиции");
+                            if (LongPos.Count == 0 && _dir != "Long" && Math.Abs(priceUpBig[bar + 1] - priceUpLittle[bar + 1]) <= DistMax.Value
+                                    && Math.Abs(priceUpBig[bar + 1] - priceUpLittle[bar + 1]) >= DistMin.Value && priceUpLittle[bar + 1] - priceDownLittle[bar + 1] <= HLittleMax.Value
+                                    && (priceDownBig[bar + 1] - priceUpLittle[bar + 1] > 0 || priceDownLittle[bar + 1] - priceUpBig[bar + 1] > 0))
+                                ShortLess(bar + 1, priceDownLittle[bar + 1] - Proboy.ValueInt * FinInfo.Security.MinStep, 1,
+                                    "Открытие короткой позиции");
+                        }
+                        // дополнить выход из позиций по логике
+                        if (ShortPos.Count > 0)
+                        {
+                            CoverAtStop(bar + 1, ShortPos[0], priceUpLittle[bar + 1] + Proboy.ValueInt * FinInfo.Security.MinStep,
+                                "Переворот.");
+                        }
+                        // дополнить выход из позиций по логике
+                        if (LongPos.Count > 0)
+                        {
+                            SellAtStop(bar + 1, LongPos[0], priceDownLittle[bar + 1] - Proboy.ValueInt * FinInfo.Security.MinStep,
+                                "Переворот.");
+                        }
+
+                        if (bar > 2)
+                        {
+                            ParamDebug("Верх. канал малый период тек.",
+                                Math.Round(ChannelUpLittle.param.LinesIndicators[0].PriceSeries[bar + 1], 4));
+                            ParamDebug("Ниж. канал малый период тек.",
+                                Math.Round(ChannelUpLittle.param.LinesIndicators[1].PriceSeries[bar + 1], 4));
+                            ParamDebug("Верх. канал большой период тек.",
+                                Math.Round(ChannelUpBig.param.LinesIndicators[0].PriceSeries[bar + 1], 4));
+                            ParamDebug("Ниж. канал большой период тек.",
+                                Math.Round(ChannelUpBig.param.LinesIndicators[1].PriceSeries[bar + 1], 4));
+                        }
+
+                        if (LongPos.Count != 0 || ShortPos.Count != 0)
+                        {
+                            if (LongPos.Count != 0)
+                                _dir = "Long";
+                            if (ShortPos.Count != 0)
+                                _dir = "Short";
+
+                            SendStandartStopFromForm(bar + 1, "");
+                            SendTimePosCloseFromForm(bar + 1, "");
+                            SendClosePosOnRiskFromForm(bar + 1, "");
+                        }
+                        else
+                        {
+                            _dir = "";
+                        }
+
+                        ParamDebug("Направление", _dir);
+                    }
+                }
+            }
+            
         }
 
         public override void GetAttributesStratetgy()
